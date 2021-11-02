f414346af9651affac5404a9e5b98f8b43487905
Emanuelle Neves <43800901+May199@users.noreply.github.com>
2021-11-02 13:15:23 -0300

    test

--- src/data/consumer.js
+++ src/data/consumer.js
@@ -2,31 +2,39 @@ const amqplib = require('amqplib/callback_api');
 const {sendEmail} = require('./sendEmail');
 
 function receiveMessage(emailClient){
-    amqplib.connect('amqp://localhost', function(err, conn) {
-        try{
-            conn.createChannel(function(err, channel) {
-                let queue = 'av2-unifor';
-            
-                channel.assertQueue(queue, {
-                  durable: false
-                });
-        
-                channel.consume(queue, function(message) {
-                  let messagePedido = JSON.parse(message.content);
-                  const msg = `<p> O pedido de número ${messagePedido.numero} com o total de R$ ${messagePedido.valor} foi cadastrado com sucesso. O prazo de entrega será até o dia ${messagePedido.data}. </p>`;
+    amqplib.connect(process.env.urlQueue, function(err, conn) {
+      if(err){
+        console.error(err);
+      }
+      conn.createChannel(function(err, channel) {
+          let queue = 'av2-unifor';
+      
+          channel.assertQueue(queue, {
+            durable: false
+          });
+  
+          channel.consume(queue, function(message) {
+            let messagePedido = JSON.parse(message.content);
+            const msg = `<p> O pedido de número ${messagePedido.numero} com o total de R$ ${messagePedido.valor} foi cadastrado com sucesso. O prazo de entrega será até o dia ${messagePedido.data}. </p>`;
 
-                  if((messagePedido.numero && messagePedido.valor && messagePedido.data) != undefined){
-                    sendEmail(emailClient, msg);
-                  } 
-                  else{
-                    console.log("E-mail não enviado!")
-                  }
-                }, {
-                  noAck: true
-                })
-            });
+            if((messagePedido.numero && messagePedido.valor && messagePedido.data) != undefined){
+              sendEmail(emailClient, msg);
+            } 
+            else{
+              console.log("E-mail não enviado!")
+            }
+          }, {
+            noAck: true
+          })
+      });
 
-        }catch(err){}
+        // try{
+
+        // }catch(err){
+        //   if (err instanceof Error) {
+        //     console.log(err);
+        //   }
+        // }
     });
 
     console.log('deu bom, passou pelo receiveMessage', emailClient);
--- src/data/producer.js
+++ src/data/producer.js
@@ -1,34 +1,34 @@
 const amqplib = require('amqplib/callback_api');
 
 function sendMessage(numero, valor, data){
-  amqplib.connect('amqp://localhost', function(err, conn){
-
-    try{
-      conn.createChannel(function(err, channel){
-        let queue = 'Trabalho AV2';
-        
-        let message = {
-          numero: numero,
-          valor: valor,
-          data: data
-        }
-        channel.assertQueue(queue, {
-          durable: false
-        });
-        channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
-
-        console.log('Message sent successfully', message);
+  amqplib.connect(process.env.urlQueue, function(err, conn){
+    if(err){
+      console.error(err);
+    }
+    conn.createChannel(function(err, channel){
+      let queue = 'Trabalho AV2';
+      
+      let message = {
+        numero: numero,
+        valor: valor,
+        data: data
+      }
+      channel.assertQueue(queue, {
+        durable: false
       });
-    }
-    catch(err){
-      return err;
-    }
+      channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
 
-    setTimeout(function() {
-      connection.close();
-      process.exit(0);
-    }, 500);
+      console.log('Message sent successfully', message);
+    });
 
+    // try{
+    // }
+    // catch(err){
+    //   if (err instanceof Error) {
+    //     console.log(err);
+    //   }
+    // }
+    setTimeout(function() { conn.close(); process.exit(0) }, 500);
   });
 
   console.log('deu bom, passou pelo sendMessage', numero, valor, data);
